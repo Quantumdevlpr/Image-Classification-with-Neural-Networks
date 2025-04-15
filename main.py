49906683eeeb5abaf1efbee236dafeea406a4815
shivansh <mshivanshmaurya@gmail.com>
2025-04-15 16:07:06 +0530

    "final commit"

--- main.py
+++ main.py
@@ -23,24 +23,5 @@ training_labels = training_labels[:20000];
 testing_images = testing_images[:4000];
 testing_labels = testing_labels[:4000];
 
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation = 'relu', input_shape = (32, 32, 3))) #this adds a convolutional layer with 32 filters of size 3*3 and relu activation function
-model.add(layers.MaxPooling2D((2, 2))) #this adds a max pooling layer with size 2*2
-model.add(layers.Conv2D(64, (3, 3), activation = 'relu')) #this adds a convolutional layer with 64 filters of size 3*3 and relu activation function
-model.add(layers.MaxPooling2D((2, 2))) #this adds a max pooling layer with size 2*2
-model.add(layers.Conv2D(64, (3, 3), activation = 'relu')) #this adds a convolutional layer with 64 filters of size 3*3 and relu activation function
-model.add(layers.Flatten()) #this flattens the output of the previous layer
-model.add(layers.Dense(64, activation = 'relu')) #this adds a dense layer with 64 neurons and relu activation function
-model.add(layers.Dense(10, activation = 'softmax')) #this adds a dense layer with 10 neurons and softmax activation function
-model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy']) #this compiles the model with adam optimizer and sparse categorical crossentropy loss function
-
-
-model.fit(training_images, training_labels, epochs = 10, validation_data = (testing_images , testing_labels)) #this fits the model on the training data with 10 epochs and batch size of 64 and validation split of 0.2
-
-loss, accuracy = model.evaluate(testing_images, testing_labels) #this evaluates the model on the testing data
-print('Loss: ', loss) #this prints the loss of the model
-print('Accuracy: ', accuracy) #this prints the accuracy of the model
-
-
-model.save('cifar10_model.h5') #this saves the model in h5 format
-models.load_model('cifar10_model.h5') #this loads the model from h5 format
+model = models.load_model('model.h5')
+# from here you can add your own image to test the model
